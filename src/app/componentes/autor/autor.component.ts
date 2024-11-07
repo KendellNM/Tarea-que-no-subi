2a3d8edb976f2ae147bbf14e29e652a229520adf
KendellNM <kendellsnm@gmail.com>
2024-11-06 19:59:22 -0500

    first commit

---
+++ src/app/componentes/autor/autor.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { NabvarComponent } from '../nabvar/nabvar.component';
+import { NgFor } from '@angular/common';
+import { Autor } from '../../models/autor';
+import { AutorService } from '../../services/autor.service';
+
+@Component({
+  selector: 'app-autor',
+  standalone: true,
+  imports: [NabvarComponent, NgFor],
+  templateUrl: './autor.component.html',
+  styleUrl: './autor.component.css'
+})
+export class AutorComponent {
+  autores:Autor[]=[];
+  constructor(
+    private autorService:AutorService
+  ){}
+  ngOnInit(){
+    this.listarAutores();
+  }
+
+  listarAutores(){
+    this.autorService.getAutores().subscribe((data)=>{
+      this.autores=data;
+    });
+  }
+  
+}
